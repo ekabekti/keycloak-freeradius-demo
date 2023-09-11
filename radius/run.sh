3ceac2e88423021fec4ccbcf7bff7fd77f27f6bd
ekabekti <newhanoman@gmail.com>
2023-09-12 01:38:56 +0700

    modified:   radius/run.sh

--- radius/run.sh
+++ radius/run.sh
@@ -4,6 +4,6 @@ docker run \
    -d \
    -t \
    --name my-radius \
-   -p 1812-1812/udp \
-   -p 1813-1813/udp \
+   -p 1812:1812/udp \
+   -p 1813:1813/udp \
     tdlabs/freeradius-server:latest \
